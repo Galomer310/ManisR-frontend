f0740bf88ff3c99c47d20fb383c96355d0cd876e
Gal Omer <galomer6708@gmail.com>
2025-03-28 23:37:02 +0300

    fixing def for render

--- src/screens/CollectFood.tsx
+++ src/screens/CollectFood.tsx
@@ -6,9 +6,6 @@ import axios from "axios";
 import "mapbox-gl/dist/mapbox-gl.css";
 import locationIcon from "../assets/location.png";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
-
 interface Meal {
   id: number;
   item_description: string;
--- src/screens/FoodUpload.tsx
+++ src/screens/FoodUpload.tsx
@@ -1,5 +1,5 @@
 // src/screens/FoodUpload.tsx
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 
 interface MealData {
